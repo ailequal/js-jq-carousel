ff2ccfa7d71763ff4523e9bd5e27ba4e0efaa8c9
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-18 17:47:08 +0100

    Main exercice done

--- main.js
+++ main.js
@@ -4,32 +4,62 @@ $(document).ready(
     // next
     $('.next').click(
       function () {
-        next();
+        clickNext();
       }
     );
     // prev
     $('.prev').click(
       function () {
-        prev();
+        clickPrev();
       }
     );
   }
 );
 
 // function next with loop and icon dot change
-function next() {
-  // remove active from img
-  $('img.active').removeClass('active');
-  // add active to following img
-  $('img.active').addClass('active');
+function clickNext() {
+  // var
+  var imageActive = $('img.active');
+  var imageNext = imageActive.next();
+  var iconActive = $('i.active');
+  var iconNext = iconActive.next();
+  // if else
+  if (imageActive.hasClass('last')) {
+    // last handler
+    imageActive.removeClass('active');
+    $('img.first').addClass('active');
+    iconActive.removeClass('active');
+    $('i.first').addClass('active');
+  } else {
+    // next one
+    imageActive.removeClass('active');
+    imageNext.addClass('active');
+    iconActive.removeClass('active');
+    iconNext.addClass('active');
+  }
 }
 
 // function prev with loop and icon dot change
-function prev() {
-  // remove active from img
-  $('img.active').removeClass('active');
-  // add active to following img
-  $('img.active').addClass('active');
+function clickPrev() {
+  // var
+  var imageActive = $('img.active');
+  var imagePrev = imageActive.prev();
+  var iconActive = $('i.active');
+  var iconPrev = iconActive.prev();
+  // if else
+  if (imageActive.hasClass('first')) {
+    // first handler
+    imageActive.removeClass('active');
+    $('img.last').addClass('active');
+    iconActive.removeClass('active');
+    $('i.last').addClass('active');
+  } else {
+    // prev one
+    imageActive.removeClass('active');
+    imagePrev.addClass('active');
+    iconActive.removeClass('active');
+    iconPrev.addClass('active');
+  }
 }
 
 // bonus adding keyboard command
